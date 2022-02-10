971794f74dcf7127b33e6d2b9d205d1b6f517b73
sihyunglee823 <sihyung_lee@tmax.com>
2022-02-10 11:45:01 +0900

    first commit

---
+++ src/main/java/com/example/echo_gradle/EchoGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.echo_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/echo_gradle/EchoGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.echo_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
